5f6b9ac18973c1f47b621e458e7297f20b1eadb7
perezgigantealex <perezgigantealex@fpllefia.com>
2024-05-31 15:56:26 +0200

    Tasca 3 finalizada Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './componentes/Header'
+import Login from './vistas/login'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Login></Login>
     </>
   )
 }
